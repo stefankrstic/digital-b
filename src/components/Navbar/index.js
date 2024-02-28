5374580d87253fe6b2dd5a5120454e127f3ea3de
stefankrstic <stefan.k.krstic@gmail.com>
2024-02-28 14:04:44 +0100

    Install fontawesome and create navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    return (
+        <>
+            <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-4 navbar-expand-lg">
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                        <Link
+                            href="/"
+                            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                        >
+                            Digital B
+                        </Link>
+                        <button
+                            type="button"
+                            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            onClick={() => setNavbarOpen(!navbarOpen)}
+                        >
+                            <i class="fa-solid fa-bars"></i>
+                        </button>
+                    </div>
+                    <div
+                        className={
+                            "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
+                            (navbarOpen ? " block rounded shadow-lg" : " hidden")
+                        }
+                    >
+                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                            <li className="flex items-center">
+                                <Link
+                                    href="/"
+                                    className="text-gray-700 px-3 py-4 lg:hover:text-gray-200 lg:py-2 flex items-center text-xs uppercase font-bold"
+                                >
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="flex items-center">
+                                <Link
+                                    href="/"
+                                    className="text-gray-700 px-3 py-4 lg:hover:text-gray-200 lg:py-2 flex items-center text-xs uppercase font-bold"
+                                >
+                                    About
+                                </Link>
+                            </li>
+                            <li className="flex items-center">
+                                <Link
+                                    href="/"
+                                    className="text-gray-700 px-3 py-4 lg:hover:text-gray-200 lg:py-2 flex items-center text-xs uppercase font-bold"
+                                >
+                                    Blog
+                                </Link>
+                            </li>
+                            <li className="flex items-center">
+                                <Link
+                                    href="/"
+                                    className="text-gray-700 px-3 py-4 lg:hover:text-gray-200 lg:py-2 flex items-center text-xs uppercase font-bold"
+                                >
+                                    Contact
+                                </Link>
+                            </li>
+                            <li className="flex items-center">
+                                <button
+                                    type="button"
+                                    className="bg-white text-gray-700 text-xs font-bold uppercase px-4 py-2 rounded active:bg-gray-50 shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                                >
+                                    <i class="fa-solid fa-phone"></i> 123456789
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-    return <main>Hello</main>;
+    return (
+        <main>
+            <Navbar />
+        </main>
+    );
 }
